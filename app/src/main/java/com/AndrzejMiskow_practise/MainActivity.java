41c7d21784a7de043b43126ec58cb35964458dbb
Andrzej Miskow <sc19am2@leeds.ac.uk>
2020-09-11 15:42:11 +0100

    Changed layout to Relative layout and created a checkbox and a button. When the button is pressed a method called start() is activited and it checks if the button is clicked.

--- app/src/main/java/com/AndrzejMiskow_practise/MainActivity.java
+++ app/src/main/java/com/AndrzejMiskow_practise/MainActivity.java
@@ -3,14 +3,39 @@ package com.AndrzejMiskow_practise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private CheckBox checkBox;
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initilisising UI elements
+        editText = (EditText) findViewById(R.id.editText);
+
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+
+    }
+
+    //checks if the checkbox is ticked or not activited by the button.
+    public void start (View view){
+        if (checkBox.isChecked()) {
+            System.out.println("It is Checked");
+        }else{
+            System.out.println("it's not Checked");
+        }
+
     }
 }
